d1efa4d6dffc84a9374591bd7ba96cb12744aa02
Bruna Tavares <bruna.tavaresr12@gmail.com>
2024-01-25 17:23:16 -0300

    adicionando a api

--- js/script.js
+++ js/script.js
@@ -1,5 +1,48 @@
 const offset = 0
 const limit = 10
-const url = "https://thronesapi.com/api/v2/Characters"
+const url = 'https://thronesapi.com/api/v2/Characters'
+
+function convertPersonagensToHtml(personagem) {
+    return `
+
+        <div id="personagemList" class="personagens2">
+            
+        
+                
+             <ul> 
+
+             <img src="${personagem.imageUrl}">
+
+                <h2> ${personagem.fullName} <h2>
 
 
+                <li>${personagem.title}</li>
+                <li>${personagem.family}</li>
+            
+              
+            </ul>
+          
+            
+        </div>
+        
+    </div>
+</section>
+`
+}
+
+
+const personagensList = document.getElementById('personagemList')
+
+
+fetch(url)
+    .then(response => response.json())
+    .then((jsonBody) => jsonBody)
+    .then((personagens) => {
+
+        for (let i = 0; i < personagens.length; i++) {
+            const personagem = personagens[i];
+            personagensList.innerHTML += convertPersonagensToHtml(personagem)
+        }
+
+    })
+    .catch((error) => console.error((error)));
